30716f9575d18bc54e8b8ae9c1bab548c3d4efaa
Ubuntu <dnguyen155@dnguyen155.yv5zdq4sslqe1jvc5wmygxteig.bx.internal.cloudapp.net>
2021-09-22 17:54:07 +0000

    Spotify song information mapping

--- main.py
+++ main.py
@@ -7,17 +7,18 @@ import flask
 import random
 
 load_dotenv(find_dotenv()) #Environment variables
-clientID = os.getenv("CLIENT_ID")
-clientSecret = os.getenv("CLIENT_SECRET")
+spotifyClientID = os.getenv("SPOTIFY_CLIENT_ID")
+spotifyClientSecret = os.getenv("SPOTIFY_CLIENT_SECRET")
 
-clientCredentials = f"{clientID}:{clientSecret}" #Spotify credentials & access token
-clientCredentialsB64 = base64.b64encode(clientCredentials.encode())
+
+spotifyClientCredentials = f"{spotifyClientID}:{spotifyClientSecret}" #Spotify credentials & access token
+spotifyClientCredentialsB64 = base64.b64encode(spotifyClientCredentials.encode())
 authorizationURL = "https://accounts.spotify.com/api/token"
 tokenData = {
     "grant_type": "client_credentials"
 }
 tokenHeaders = {
-    "Authorization": f"Basic {clientCredentialsB64.decode()}"
+    "Authorization": f"Basic {spotifyClientCredentialsB64.decode()}"
 }
 authorizationResponse = requests.post(authorizationURL, data=tokenData, headers=tokenHeaders)
 authorizationResponseData = authorizationResponse.json()
@@ -26,18 +27,41 @@ headers = {
     "Authorization": "Bearer " +accessToken
 }
 
-ids = ['1EqmdzkoxPJlQP039YoGCq', '4iJLPqClelZOBCBifm8Fzv', '7H55rcKCfwqkyDFH9wpKM6'] #Vivid Undress, Pierce the Veil, Christina Perri
+ids = ['6Wr3hh341P84m3EI8qdn9O', '4iJLPqClelZOBCBifm8Fzv', '7H55rcKCfwqkyDFH9wpKM6'] #Rise Against, Pierce the Veil, Christina Perri
 queries = "?market=US" 
-topSongs = []
-randomSongs = []
+randomImagePreview =[]
+randomSongName = []
+randomSongLink = []
+randomSongPreviewURL = []
+randomArtistName = []
+randomArtistLink = []
+randomAlbumLink = []
+
 for id in ids: #Artists' top track (US)
     endpoint = f"https://api.spotify.com/v1/artists/{id}/top-tracks"
     endpointQueries = f"{endpoint}{queries}"
     result = requests.get(url=endpointQueries, headers=headers)
+    randomSelect = random.randint(0, 9) #Random 
+    y = 0
     for x in result.json()['tracks']:
-        topSongs.append(x['name'])
-    randomSongs.append(topSongs[random.randint(0, len(topSongs)-1)])
-print(randomSongs)
+        if(y == randomSelect):
+            randomImagePreview.append(x['album']['images'][0]['url']) 
+            randomSongName.append(x['name']) 
+            randomSongLink.append(x['external_urls']['spotify']) 
+            randomSongPreviewURL.append(x['preview_url']) 
+            randomArtistName.append(x['artists'][0]['name']) 
+            randomArtistLink.append(x['album']['artists'][0]['external_urls']['spotify'])
+            randomAlbumLink.append(x['album']['external_urls']['spotify']) 
+            break
+        y += 1
+print(randomImagePreview)
+print(randomSongName)
+print(randomSongLink)
+print(randomSongPreviewURL)
+print(randomArtistName)
+print(randomArtistLink)
+print(randomAlbumLink)
+
 
 ##app = flask.Flask(__name__)
 
