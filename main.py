acbf48eceb91b6313f477155cbb95d5597360970
Ubuntu <dnguyen155@dnguyen155.yv5zdq4sslqe1jvc5wmygxteig.bx.internal.cloudapp.net>
2021-09-22 15:58:12 +0000

    3 spotify artist ID queries with their top song parsed

--- main.py
+++ main.py
@@ -20,13 +20,26 @@ tokenHeaders = {
 authorizationResponse = requests.post(authorizationURL, data=tokenData, headers=tokenHeaders)
 authorizationResponseData = authorizationResponse.json()
 accessToken = authorizationResponseData['access_token']
+ids = ['1EqmdzkoxPJlQP039YoGCq', '4iJLPqClelZOBCBifm8Fzv', '7H55rcKCfwqkyDFH9wpKM6'] 
+queries = "?market=US"
+headers = {
+    "Authorization": "Bearer " +accessToken
+}
+for id in ids:
+    endpoint = f"https://api.spotify.com/v1/artists/{id}/top-tracks"
+    endpointQueries = f"{endpoint}{queries}"
+    result = requests.get(url=endpointQueries, headers=headers)
+    for x in result.json()['tracks']:
+        topSong = (x['name'])
+        print(topSong)
+        break
 
-app = flask.Flask(__name__)
+##app = flask.Flask(__name__)
 
-@app.route("/")
-def index():
-    return flask.render_template("index.html")
+##@app.route("/")
+##def index():
+##    return flask.render_template("index.html")
 
-app.run(
-    debug=True
-)
+##app.run(
+##    debug=True
+##)
